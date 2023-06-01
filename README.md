81a0a085e9455ffa636a062a9fa6982060c9ff80
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2023-06-01 14:22:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Install dependencies using `pip install -r requirements.txt` .
+2. Type `ipconfig/` in terminal and copy and paste ipv4 address in the url value in [line number 19](./static/index.js) of ajax call in index.js file
+3. Run `python app.py` in terminal
+4. Now, flask localserver opens up in console, click it.
---
+++ static/index.js
@@ -0,0 +1,146 @@
+let englishtext = "";
+let hinditext = "";
+
+// No need of progress bar for time delay in app version-2 but kept it due to nice animation.
+
+window.onload = () => {
+	$('#sendbutton').click(() => {
+		englishtext = "";
+		hinditext = "";
+		hideInterface();
+
+		imagebox = $('#imagebox')
+		input = $('#imageinput')[0]
+		if(input.files && input.files[0])
+		{
+			let formData = new FormData();
+			formData.append('image' , input.files[0]);
+			$.ajax({
+				url: "http://192.168.26.195:8080/detectObject", 
+				// fix below to your liking
+				// url: "http://xxx.xxx.xxx.xxx:8080/detectObject", 
+				type:"POST",
+				data: formData,
+				cache: false,
+				processData:false,
+				contentType:false,
+				error: function(data){
+					console.log("upload error" , data);
+					console.log(data.getAllResponseHeaders());
+
+					updateInterface();
+				},
+				success: function(data){
+					console.log(data);
+					bytestring = data['status'];
+					image = bytestring.split('\'')[1];
+					englishtext = data['englishmessage'];
+					hinditext = data['hindimessage'];
+					imagebox.attr('src' , 'data:image/jpeg;base64,'+image);
+					
+					// $('#audio').html('<audio autoplay><source src="static/detected_image.mp3"></audio>');
+					updateInterface();					
+				}
+			});
+			
+		}
+	});
+
+	// below speechSynthesis WebAPI
+	const voiceEnglishOutputButton = document.getElementById('voice-english-output');
+	const voiceHindiOutputButton = document.getElementById('voice-hindi-output');
+	voiceEnglishOutputButton.addEventListener('click', () => {
+		let utterance = new SpeechSynthesisUtterance(englishtext);
+		// utterance.lang = "en-US";
+		utterance.lang = "en-GB";
+		// utterance.voice = speechSynthesis.getVoices()[2]; // MS Zira
+		// utterance.voice = speechSynthesis.getVoices()[4]; // Google English
+		utterance.voice = speechSynthesis.getVoices()[5];// Google English Female
+		speechSynthesis.speak(utterance);
+	});
+	voiceHindiOutputButton.addEventListener('click', () => {
+		let utterance = new SpeechSynthesisUtterance(hinditext);
+		utterance.lang = "hi-IN";
+		utterance.voice = speechSynthesis.getVoices()[10];// Google Hindi Female
+		speechSynthesis.speak(utterance);
+	});
+};
+
+
+
+function readUrl(input){
+	imagebox = $('#imagebox');
+	console.log("evoked readUrl");
+	if(input.files && input.files[0]){
+		let reader = new FileReader();
+		reader.onload = function(e){
+			// console.log(e)
+			
+			imagebox.attr('src',e.target.result); 
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+
+function hideInterface(){
+	$(".loading").hide();
+	let progresstext = document.querySelector('.text');
+	progresstext.style.display = "none";//hide completed text
+	hideButtons();
+	function hideButtons(){
+		$("#voice-english-output").hide();
+		$("#voice-hindi-output").hide();
+	}
+}
+
+function updateInterface(){
+	$(".loading").show();
+	progress();
+
+	// show voice output after 18s approx
+	setTimeout(
+		function () {
+			showTarget();
+		}, 10000
+	);
+
+	function showTarget() {
+		$("#voice-english-output").show();
+		$("#voice-hindi-output").show();
+	}
+}
+
+function progress() {
+	let percent = document.querySelector('.percent');
+	let progress = document.querySelector('.progress');
+	let text = document.querySelector('.text');
+	let count = 12;//4
+	let per = 8;//16
+	let loading = setInterval(animateProgress, 100);
+
+	function animateProgress() {
+		if (count == 100 && per == 360) {
+			percent.classList.add("text-blink")
+			percent.innerText = "Audio File Generated!! Click Get Speech Out"
+			percent.style.fontSize = "20px";
+
+			text.style.display = "block";
+			clearInterval(loading);
+		}
+		else {
+			per = per + 4;
+			count = count + 1;
+			progress.style.width = per + 'px';
+			// percent.textContent = count + '%';
+			percent.innerText = count + '%';
+		}
+	}
+}
+
+function changeColor(){
+	let sendButton = document.querySelector("#sendbutton");
+	// let sendButton = document.getElementById("sendbutton");
+	sendButton.style.backgroundColor = "orange";
+	sendButton.style.color = "black";
+}
